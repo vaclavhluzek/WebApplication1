879ad30571694bb660bb1f000b18c3c310df63c1
Vaclav Hluzek <vaclav.hluzek@mainstream.cz>
2023-10-12 15:11:03 +0200

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Web.Mvc;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        // GET: HelloWorld
+        [HttpGet]
+        public String Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
